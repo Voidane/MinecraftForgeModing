eec49173ef6caa521eca43a8fad8df031df602c3
Voidane <hero4aquest@gmail.com>
2024-04-19 10:55:56 -0400

    New global loot drops

---
+++ src/main/java/net/voidane/testmod/datagen/ModGlobalLootModifiersProvider.java
@@ -0,0 +1,40 @@
+package net.voidane.testmod.datagen;
+
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
+import net.minecraftforge.common.data.GlobalLootModifierProvider;
+import net.minecraftforge.common.loot.LootTableIdCondition;
+import net.voidane.testmod.TestMod;
+import net.voidane.testmod.item.ModItems;
+import net.voidane.testmod.loot.AddItemModifier;
+
+public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
+
+    public ModGlobalLootModifiersProvider(PackOutput output) {
+        super(output, TestMod.MOD_ID);
+    }
+
+    @Override
+    protected void start() {
+
+        // Allow grass to drop from grass with a 5% chance drop rate.
+        add("strawberry_from_grass", new AddItemModifier(new LootItemCondition[]{
+                LootItemBlockStatePropertyCondition.hasBlockStateProperties(Blocks.GRASS).build(),
+                LootItemRandomChanceCondition.randomChance(0.05f).build()
+                }   , ModItems.STRAWBERRY.get()));
+
+        // Allow creepers to drop strawberries every time we kill them
+        add("strawberry_from_creeper", new AddItemModifier(new LootItemCondition[]{
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/creeper")).build()
+                }   , ModItems.STRAWBERRY.get()));
+
+        // Add a metal detector to a jungle temple chest with 100% drop rate
+        add("metal_detector_from_jungle_temples", new AddItemModifier(new LootItemCondition[]{
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/jungle_temple")).build()
+        }, ModItems.METAL_DETECTOR.get()));
+    }
+}
---
+++ src/main/java/net/voidane/testmod/loot/ModLootModifiers.java
@@ -0,0 +1,23 @@
+package net.voidane.testmod.loot;
+
+import com.mojang.serialization.Codec;
+import net.minecraftforge.common.loot.IGlobalLootModifier;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.voidane.testmod.TestMod;
+
+public class ModLootModifiers {
+
+    public static final DeferredRegister<Codec<? extends IGlobalLootModifier>> LOOT_MODIFIER_SERIALIZERS =
+            DeferredRegister.create(ForgeRegistries.Keys.GLOBAL_LOOT_MODIFIER_SERIALIZERS, TestMod.MOD_ID);
+
+    public static final RegistryObject<Codec<? extends IGlobalLootModifier>> ADD_ITEM =
+            LOOT_MODIFIER_SERIALIZERS.register("add_item", AddItemModifier.CODEC);
+
+    public static void registerMethod(IEventBus eventBus) {
+        LOOT_MODIFIER_SERIALIZERS.register(eventBus);
+    }
+
+}
